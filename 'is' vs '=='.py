bae35dee2997db6ecdb243787492f7a2304919ed
Srinath Shretha <srinathshrestha9890@gmail.com>
2021-01-23 20:44:19 +0530

    date

---
+++ 'is' vs '=='.py
@@ -0,0 +1,2 @@
+# == : value equality - 2 object have the same value
+# is : referances equality - two referance refer to the same object
--- list.py
+++ list.py
@@ -1,6 +1,8 @@
-grocery = ['deo','chicken','milk','eggs','sprouts','mug dal' , 199]
-print(grocery[6])
-numbers = [2,5,1,67,9,1]
+# grocery = ['deo','chicken','milk','eggs','sprouts','mug dal' , 199]
+# print(grocery[6])
+# numbers = [2,5,1,67,9,1]
+# list_1 =['f','srinath']
+
 # a = numbers.sort() # its unnecessry to write a function for sorting insted of using the inbuit one
 # b = numbers.reverse()
 # print(numbers[1::2]) # but doing these actions wil not effect aur original iist
@@ -14,20 +16,32 @@ numbers = [2,5,1,67,9,1]
 # numbers.remove(9) # 9 will get removed from the list
 # numbers.pop() # this wi remove the end iteam from the list.
 # numbers[1]=32
-print(numbers)
+# print(numbers[2:]) #print elements starting from 3rd elements in the list
+# print(numbers[1:3]) # print elements 2nd and 3rd
+# print(numbers*2) # prints the list numbers twice
+# print((numbers+list_1)) # provides such a list where there is elements of both numbers and list_1 ( respectivly)
+
 # TUPPLES
-'''mutable - can change
-immutable- can't change'''
-tupple = (3,2,5,1,5)
-print(tupple)
-tp=(1,) # wee need to put a , after the charecter if u have one iteam in ur tupple
-print(tp)
+# '''mutable - can change
+# immutable- can't change'''
+# tupple = (3,2,5,1,5)
+# print(tupple)
+# tp=(1,) # wee need to put a , after the charecter if u have one iteam in ur tupple
+# print(tp)
 
 # SWAPPING
-a = 1
-b =3
-a,b = b,a
+# a = 1
+# b =3
+# a,b = b,a
 # temp = a
 # a=b
 # b = temp
-print(a,b)
+# print(a,b)
+#-------------------------------------------------question----------------------------------------
+# question to input 10 nums in a list and then display all the elements by multiplying 2
+# print('Enter the elements of these list : ')
+# lis =[]
+# for i in range(0,5):
+#     a = int(input())
+#     lis.append(a*2)
+# print(lis)
